495cd4f21d5ebcb588b82a80ba899e696116abe1
Aaron Magat <aaronmgt@gmail.com>
2016-04-10 23:30:47 -0700

    Adding the client and server directories

---
+++ src/TaoClient/TaoClient.java
@@ -0,0 +1,17 @@
+package TaoClient;
+
+/**
+ * @brief Class to represent the client of TaoStore
+ */
+public class TaoClient {
+    /**
+     * @brief Default constructor
+     */
+    public TaoClient() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/TaoServer/TaoServer.java
@@ -0,0 +1,16 @@
+package TaoServer;
+
+/**
+ * @brief Class to represent a server for TaoStore
+ */
+public class TaoServer {
+    /**
+     * @brief Default constructor
+     */
+    public TaoServer() {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
