f9979fe7e3a256cba1278504d7355f139d694611
Jeffrey Helt <jmhelt@users.noreply.github.com>
2020-10-29 16:56:52 -0400

    Modify build for CloudLab.

---
+++ start_client.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+java -classpath ./out/production/TaoStore:./libs/guava-19.0.jar:./libs/commons-math3-3.6.1.jar:./libs/junit-4.11.jar TaoClient.TaoClient
---
+++ start_server.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+java -classpath ./out/production/TaoStore/:./libs/commons-math3-3.6.1.jar:./libs/guava-19.0.jar:./libs/junit-4.11.jar TaoServer.TaoServer
